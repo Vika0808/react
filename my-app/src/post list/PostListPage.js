8ec037875ecdea62f50c06cd7752a1c8d0f8e323
Vika0808 <toraline77@gmail.com>
2024-05-07 11:18:10 +0300

    Adding posts to the list with generated indexes

--- my-app/src/post list/PostListPage.js
+++ my-app/src/post list/PostListPage.js
@@ -54,40 +54,40 @@ const generatePosts = (count) => {
 const posts = initialPosts.concat(generatePosts(7)); 
 
 const PostListPage = () => {
-    return (
-        <div>
-            <header className="header">
-              <h1>Пости</h1>
-              <Link to="/registration">
-                <button className="registration-button">Реєстрація/Вхід</button>
-              </Link>
-            </header>
-            <div className="post-list-container">
-                <div className="post-list">
-                    {posts.map(post => (
-                        <div key={post.id} className="post-item">
-                            <h3 className="post-title">{post.title}</h3>
-                            {post.text && <p className="post-text">{post.text}</p>}
-                            {post.comments.length > 0 && (
-                                <ul className="comment-list">
-                                    {post.comments.map(comment => (
-                                        <li key={comment.commentId} className="comment-item">{comment.commentText}</li>
-                                    ))}
-                                </ul>
-                            )}
-                            <div className="detail-button-box">
-                                <Link to={`/posts/${post.id}`}>
-                                    <button className="detail-button">
-                                        Детальніше
-                                    </button>
-                                </Link>
-                            </div>
-                        </div>
-                    ))}
-                </div>
-            </div>
-        </div>
-    );
+  return (
+      <div>
+          <header className="header">
+            <h1>Пости</h1>
+            <Link to="/registration">
+              <button className="registration-button">Реєстрація/Вхід</button>
+            </Link>
+          </header>
+          <div className="post-list-container">
+              <div className="post-list">
+                  {posts.map(post => (
+                      <div key={post.id} id={post.id} className="post-item"> {}
+                          <h3 className="post-title">{post.title}</h3>
+                          {post.text && <p className="post-text">{post.text}</p>}
+                          {post.comments.length > 0 && (
+                              <ul className="comment-list">
+                                  {post.comments.map(comment => (
+                                      <li key={comment.commentId} className="comment-item">{comment.commentText}</li>
+                                  ))}
+                              </ul>
+                          )}
+                          <div className="detail-button-box">
+                              <Link to={`/posts/${post.id}`}>
+                                  <button className="detail-button">
+                                      Детальніше
+                                  </button>
+                              </Link>
+                          </div>
+                      </div>
+                  ))}
+              </div>
+          </div>
+      </div>
+  );
 };
 
 export default PostListPage;
